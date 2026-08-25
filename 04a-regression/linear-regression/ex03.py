e3ae966fa1526b007cc4bcacaefbb1c5d5706793
isnika <Ngkhanhhuyen67@gmail.com>
2026-08-25 14:59:41 +0700

    add ex

---
+++ 04a-regression/linear-regression/ex03.py
@@ -0,0 +1,43 @@
+'''
+Bài 18 — Linear Regression bằng sklearn
+Cho dataset:
+X = [[1], [2], [3], [4], [5]]
+y = [2, 4, 6, 8, 10]
+Sử dụng:
+LinearRegression
+để:
+Train model.
+In w.
+In b.
+Dự đoán x = 6.
+Tính MSE.
+Tính R².
+'''
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+#Date set
+X = [[1], [2], [3], [4], [5]]
+y = [2, 4, 6, 8, 10]
+
+#Tạo model
+model = LinearRegression()
+#train model
+model.fit(X, y)
+# in w
+print("Giá trị w = ", model.coef_[0])
+#in b
+print("Giá trị b = ", model.intercept_)
+#Dự đoán x =6
+y_pred = model.predict([[6]])
+print("Dự đoán y-hat khi x = 6: ",y_pred[0])
+
+#Dự đoán toàn bộ dữ liệu
+y_predict = model.predict(X)
+
+#Tính MSE
+mse = mean_squared_error(y, y_predict)
+print("MSE = ", mse)
+#Tính r^2
+r2 = r2_score(y, y_predict)
+print("R^2 = ", r2)
